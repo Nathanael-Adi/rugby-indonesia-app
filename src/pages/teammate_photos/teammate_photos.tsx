29f6ac091c435dd4129b33fe718f40bdef036ada
Nathanael-Adi <6181901041@student.unpar.ac.id>
2024-05-18 11:06:12 +0700

    upload photos with frame ok

--- src/pages/teammate_photos/teammate_photos.tsx
+++ src/pages/teammate_photos/teammate_photos.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonModal, IonCol } from '@ionic/react';
 import { Camera, Photo, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Directory, Filesystem } from '@capacitor/filesystem';
 
 import './teammate_photos.css';
 
@@ -28,21 +27,20 @@ const frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, 
 const TeammatePage: React.FC = () => {
     const [images, setImages] = useState<PhotoImages[]>([]);
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedFrame, setSelectedFrame] = useState<string | null>(null);
     const [takenPhoto, setTakenPhoto] = useState<Photo | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            try {            
+            try {
                 const response = await fetch('https://dnartworks.rugbyindonesia.or.id/indonesianrugby/photos/list.json');
                 const data = await response.json();
                 const result = data.data;
-    
+
                 if (!Array.isArray(result)) {
                     throw new Error('Data is not in expected format');
                 }
 
-                const photoUrls = result.map((item: any) =>{
+                const photoUrls = result.map((item: any) => {
                     const photoUrl = item;
                     const modPhotoUrl = photoUrl.replace("/images/", "/");
                     return modPhotoUrl;
@@ -63,7 +61,7 @@ const TeammatePage: React.FC = () => {
             allowEditing: false,
             resultType: CameraResultType.Base64
         });
-        
+
         setTakenPhoto(image);
         setIsOpen(true);  // Buka modal setelah foto diambil
     };
@@ -80,7 +78,7 @@ const TeammatePage: React.FC = () => {
         setIsOpen(true);  // Buka modal setelah foto dipilih
     };
 
-    const editPhoto = async (base64data: string): Promise<string> => {
+    const editPhoto = async (base64data: string, frame: string): Promise<string> => {
         return new Promise((resolve) => {
             const canvas = document.createElement('canvas');
             const ctx = canvas.getContext('2d');
@@ -91,16 +89,11 @@ const TeammatePage: React.FC = () => {
                 canvas.width = 400;
                 canvas.height = 400;
                 ctx!.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-                if (selectedFrame) {
-                    frameImg.src = selectedFrame;
-                    frameImg.onload = () => {
-                        ctx!.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
-                        resolve(canvas.toDataURL('image/jpeg'));
-                    };
-                } else {
+                frameImg.src = frame;
+                frameImg.onload = () => {
+                    ctx!.drawImage(frameImg, 0, 0, canvas.width, canvas.height);
                     resolve(canvas.toDataURL('image/jpeg'));
-                }
+                };
             };
 
             img.src = `data:image/jpeg;base64,${base64data}`;
@@ -130,9 +123,8 @@ const TeammatePage: React.FC = () => {
     };
 
     const handleFrameSelection = async (frame: string) => {
-        setSelectedFrame(frame);
         if (takenPhoto) {
-            const editedBase64Data = await editPhoto(takenPhoto.base64String!);
+            const editedBase64Data = await editPhoto(takenPhoto.base64String!, frame);
             await uploadPhoto(editedBase64Data);
         }
         setIsOpen(false);
@@ -145,7 +137,7 @@ const TeammatePage: React.FC = () => {
                     <IonButtons slot="start">
                         <a href='/latest_news'>
                             <img src={homeIcon} alt="home-icon" className='home-icon' />
-                        </a>                   
+                        </a>
                     </IonButtons>
                     <IonButtons slot="end">
                         <IonMenuButton />
@@ -187,14 +179,14 @@ const TeammatePage: React.FC = () => {
                             {frames.map((frame, index) => (
                                 <IonCol size = "6" key={index}>
                                     <img
-                                    key={index}
-                                    src={frame}
-                                    alt={`Frame ${index + 1}`}
-                                    className={`frame-option ${selectedFrame === frame ? 'selected' : ''}`}
-                                    onClick={() => handleFrameSelection(frame)}
-                                    width={200}
-                                    height={200}
-                                />
+                                        key={index}
+                                        src={frame}
+                                        alt={`Frame ${index + 1}`}
+                                        className="frame-option"
+                                        onClick={() => handleFrameSelection(frame)}
+                                        width={200}
+                                        height={200}
+                                    />
                                 </IonCol>
                             ))}
                         </div>
